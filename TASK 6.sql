f07aa27452ced05755165410b1a7d278a5dd3ab4
Vaqif <158089331+vqasimov12@users.noreply.github.com>
2024-07-07 23:20:52 +0400

    View Task

---
+++ TASK 6.sql
@@ -0,0 +1,26 @@
+GO
+USE Teachers
+GO
+
+
+--TASK 1
+--GO
+--CREATE VIEW AllJobTitles AS
+--SELECT [Name]
+--FROM Posts
+--GO
+
+SELECT *
+FROM AllJobTitles
+
+--TASK 2
+GO
+--CREATE VIEW AllTeacherNames AS
+--SELECT [Name]
+--FROM Teachers_Table
+--GO
+
+SELECT* 
+FROM AllTeacherNames
+
+
